25648774753f8438aacf143401b65807ec439db3
sunchuvinod000 <sunchuvinod000@gmail.com>
2025-03-19 11:44:25 +0530

    increased rate limit

--- index.js
+++ index.js
@@ -30,8 +30,8 @@ const port = process.env.PORT || 5000;
 
 // Rate limiter
 const limiter = rateLimit({
-    windowMs: 60 * 1000, // 15 minutes
-    max: 2, // limit each IP to 100 requests per windowMs
+    windowMs: 60 * 1000, // 1 minutes
+    max: 20, // limit each IP to 100 requests per windowMs
     message: "Too many requests from this IP, please try again after a minute"
 });
 // Set hbs as the template engine
